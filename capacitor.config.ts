d28a6859e9b2136a63d3d193b5f1b912e5804bd9
sithu94-c2po <saun2162@gmail.com>
2025-10-27 20:48:03 +1100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.c2.skip',
+  appName: 'Skip',
+  webDir: 'dist'
+};
+
+export default config;
